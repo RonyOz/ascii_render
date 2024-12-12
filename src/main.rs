d9d67299807fa9656eddf4bbcc87bac055143ea5
Rony Ordoñez <ronyfarido@gmail.com>
2024-12-11 23:52:05 -0500

    Refactor WebSocket handling to use SSE for scrolling text and integrate ASCII art processing

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,38 @@ mod base {
     pub mod ascii; // This declares a module named ascii in the base module.
 }
 
-use warp::{filters::ws::{Message, WebSocket}, Filter}; // warp is a web server framework in Rust, and Filter is a trait that defines the core functionality of a filter.
-use futures::sink::SinkExt; // Import the SinkExt trait to use the send method on WebSocket.
+use base::{ascii::asciify, scroll};
+use warp::Filter; // warp is a web server framework in Rust, and Filter is a trait that defines the core functionality of a filter.
+use tokio::time::{Duration, sleep};
 
 #[tokio::main] // This is an attribute that tells the Rust compiler to generate the main function that uses the Tokio runtime.
 async fn main() { 
 
-    let ws_route = warp::path!(String)
-        .and(warp::ws())
-        .map(|text: String, ws: warp::ws::Ws| {
-            ws.on_upgrade(move |socket| scroll_text(socket, text))
+    let stream_route = warp::path!(String)
+        .map(|text: String|{
+            let stream = warp::sse::reply(warp::sse::keep_alive().stream(async_stream::stream!{
+                let mut scrolling_text = match asciify(&text, "broadway").await {
+                    Ok(text) => text,
+                    Err(_) => return,
+                };
+                loop {
+
+                    // Send a screen cleaner
+                    yield Ok::<_, warp::Error>(warp::sse::Event::default().data("\x1B[2J\x1B[1;1H"));
+
+                    scrolling_text = scroll::scroll_text(&scrolling_text, 10).await;
+
+                    yield Ok::<_, warp::Error>(warp::sse::Event::default().data(scrolling_text.clone()));
+                    sleep(Duration::from_millis(500)).await;
+                } 
+            }));
+
+            stream
         });
 
-    warp::serve(ws_route) 
+    warp::serve(stream_route) 
         .run(([127, 0, 0, 1], 3030)) 
         .await; // wait for the server to finish running.
 
 }
-async fn scroll_text(mut socket: WebSocket, text: String) {
-    let mut scrolling_text = text;
 
-    loop {
-        clean_screen();
-        if socket.send(Message::text(&scrolling_text)).await.is_err() {
-            break;
-        }
-    }
-}
-
-fn clean_screen() {
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-}
